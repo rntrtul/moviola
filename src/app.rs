7170892920b07847e837b37b5187744fc27bf7c0
Fareed <sistthelord@gmail.com>
2024-06-09 15:39:06 -0400

    Video choose logic in main app

--- src/app.rs
+++ src/app.rs
@@ -3,8 +3,8 @@ use relm4::{adw, gtk, main_application, ComponentParts, ComponentSender, SimpleC
 use gtk::prelude::{
     ApplicationExt, GtkWindowExt, OrientableExt, WidgetExt};
 use gtk::{glib};
-use gtk4::{gio, Window};
-use gtk4::prelude::{ButtonExt, FileExt, BoxExt};
+use gtk4::{gio};
+use gtk4::prelude::{ButtonExt, FileExt, GtkApplicationExt};
 use super::ui::video_player::{VideoPlayerModel, VideoPlayerMsg};
 use super::ui::edit_controls::EditControlsModel;
 
@@ -16,14 +16,50 @@ pub(super) struct App {
 
 #[derive(Debug)]
 pub(super) enum AppMsg {
+    OpenFile,
+    SetVideo(String),
     Quit,
+}
+
+impl App {
+    fn launch_file_opener(sender: ComponentSender<Self>) {
+        let filters = gio::ListStore::new::<gtk::FileFilter>();
+
+        let video_filter = gtk::FileFilter::new();
+        video_filter.add_mime_type("video/*");
+        video_filter.set_name(Some("Video"));
+        filters.append(&video_filter);
+
+        let audio_filter = gtk::FileFilter::new();
+        audio_filter.add_mime_type("audio/*");
+        audio_filter.set_name(Some("Audio"));
+        filters.append(&audio_filter);
+
+        let file_dialog = gtk::FileDialog::builder()
+            .title("Open Video")
+            .accept_label("Open")
+            .modal(true)
+            .filters(&filters)
+            .build();
+
+        let cancelable = gio::Cancellable::new();
+        let window = relm4::main_adw_application().active_window().unwrap();
+
+        file_dialog.open(Some(&window), Some(&cancelable), move |result| {
+            let file = match result {
+                Ok(f) => f,
+                Err(_) => return,
+            };
+            sender.input(AppMsg::SetVideo(file.uri().to_string()));
+        });
+    }
 }
 
 #[relm4::component(pub)]
 impl SimpleComponent for App {
-    type Init = u8;
     type Input = AppMsg;
     type Output = ();
+    type Init = u8;
     view! {
         main_window = adw::ApplicationWindow::new(&main_application()) {
             set_visible: true,
@@ -43,7 +79,25 @@ impl SimpleComponent for App {
                 gtk::Box{
                     set_orientation: gtk::Orientation::Vertical,
 
-                    model.video_player.widget(),
+                    adw::StatusPage {
+                        set_title: "Select Video",
+                        set_description: Some("select a video file to edit"),
+
+                        #[name = "open_file_btn"]
+                        gtk::Button {
+                            set_label: "Open File",
+                            set_hexpand: false,
+                            add_css_class: "suggested-action",
+                            add_css_class: "pill",
+                        },
+
+                        #[watch]
+                        set_visible: !model.video_is_open,
+                    },
+
+                    model.video_player.widget(){
+                        set_visible: model.video_is_open,
+                    },
 
                     #[name="stack"]
                     adw::ViewStack {
@@ -59,13 +113,7 @@ impl SimpleComponent for App {
         }
     }
 
-    type Widgets = AppWidgets;
-
-    fn init(
-        _: Self::Init,
-        root: Self::Root,
-        sender: ComponentSender<Self>,
-    ) -> ComponentParts<Self> {
+    fn init(_: Self::Init, root: Self::Root, sender: ComponentSender<Self>) -> ComponentParts<Self> {
         let video_player: Controller<VideoPlayerModel> =
             VideoPlayerModel::builder()
                 .launch(2)
@@ -88,12 +136,22 @@ impl SimpleComponent for App {
         widgets.switch_bar.set_stack(Some(&widgets.stack));
         widgets.tool_bar_view.set_content(Some(&widgets.content));
 
+        widgets.open_file_btn.connect_clicked(move |_| {
+            sender.input(AppMsg::OpenFile);
+        });
+
         ComponentParts { model, widgets }
     }
 
     fn update(&mut self, message: Self::Input, _sender: ComponentSender<Self>) {
         match message {
             AppMsg::Quit => main_application().quit(),
+            AppMsg::OpenFile => App::launch_file_opener(_sender),
+            AppMsg::SetVideo(file_name) => {
+                self.video_player.emit(VideoPlayerMsg::NewVideo(file_name));
+                self.video_player.widget().set_visible(true);
+                self.video_is_open = true;
+            }
         }
     }
 }
--- src/ui/video_player.rs
+++ src/ui/video_player.rs
@@ -20,40 +20,6 @@ pub enum VideoPlayerMsg {
     NewVideo(String),
 }
 
-fn launch_file_opener(sender: ComponentSender<VideoPlayerModel>) {
-    let filters = gio::ListStore::new::<gtk::FileFilter>();
-
-    let video_filter = gtk::FileFilter::new();
-    video_filter.add_mime_type("video/*");
-    video_filter.set_name(Some("Video"));
-    filters.append(&video_filter);
-
-    let audio_filter = gtk::FileFilter::new();
-    audio_filter.add_mime_type("audio/*");
-    audio_filter.set_name(Some("Audio"));
-    filters.append(&audio_filter);
-
-    let file_dialog = gtk::FileDialog::builder()
-        .title("Open Video")
-        .accept_label("Open")
-        .modal(true)
-        .filters(&filters)
-        .build();
-
-    let cancelable = gio::Cancellable::new();
-    let window = relm4::main_adw_application().active_window().unwrap();
-    // let window = root.root().unwrap().downcast::<Window>().unwrap();
-
-    file_dialog.open(Some(&window), Some(&cancelable), move |result| {
-        let file = match result {
-            Ok(f) => f,
-            Err(_) => return,
-        };
-        let file_uri = file.uri().as_str().to_string();
-        sender.input(VideoPlayerMsg::NewVideo(file_uri));
-    })
-}
-
 #[relm4::component(pub)]
 impl SimpleComponent for VideoPlayerModel {
     type Input = VideoPlayerMsg;
@@ -66,21 +32,6 @@ impl SimpleComponent for VideoPlayerModel {
             set_width_request: 670,
             set_height_request: 390,
             inline_css: "margin: 15px",
-            adw::StatusPage {
-                set_title: "Select Video",
-                set_description: Some("select a video file to edit"),
-
-                #[name = "open_file_btn"]
-                gtk::Button {
-                    set_label: "Open File",
-                    set_hexpand: false,
-                    add_css_class: "suggested-action",
-                    add_css_class: "pill",
-                },
-
-                #[watch]
-                set_visible: !model.video_is_selected,
-            },
 
             #[name = "vid_frame"]
             gtk::Box {
@@ -120,10 +71,6 @@ impl SimpleComponent for VideoPlayerModel {
         let widgets = view_output!();
 
         widgets.vid_frame.append(&offload);
-
-        widgets.open_file_btn.connect_clicked(glib::clone!(@weak root =>  move |_| {
-            launch_file_opener(sender.clone());
-        }));
 
         ComponentParts { model, widgets }
     }
