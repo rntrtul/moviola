beae3196bb2c76d6fe5ad0ebca40e07f8983a44b
Fareed <sistthelord@gmail.com>
2024-07-10 18:29:29 -0400

    Move Handle code out of mod folder

--- src/ui/handle.rs
+++ src/ui/handle.rs
@@ -111,3 +111,25 @@ impl HandleWidget {
         self.target_x.set(pos);
     }
 }
+
+impl crate::ui::HandleWidget {
+    fn new(x: i32, is_handle: bool, is_start: bool) -> Self {
+        glib::Object::builder()
+            .property("x", x)
+            .property("rel_x", 0)
+            .property("is_handle", is_handle)
+            .property("is_start", is_start)
+            .build()
+    }
+}
+
+impl Default for crate::ui::HandleWidget {
+    fn default() -> Self {
+        glib::Object::builder()
+            .property("x", 0)
+            .property("rel_x", 0)
+            .property("is_handle", true)
+            .property("is_start", true)
+            .build()
+    }
+}
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -18,26 +18,3 @@ glib::wrapper! {
     pub struct CropBoxWidget(ObjectSubclass<crop_box::CropBoxWidget>)
         @extends gtk::Widget;
 }
-
-// todo: put in handle file?
-impl HandleWidget {
-    fn new(x: i32, is_handle: bool, is_start: bool) -> Self {
-        glib::Object::builder()
-            .property("x", x)
-            .property("rel_x", 0)
-            .property("is_handle", is_handle)
-            .property("is_start", is_start)
-            .build()
-    }
-}
-
-impl Default for HandleWidget {
-    fn default() -> Self {
-        glib::Object::builder()
-            .property("x", 0)
-            .property("rel_x", 0)
-            .property("is_handle", true)
-            .property("is_start", true)
-            .build()
-    }
-}
