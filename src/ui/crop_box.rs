f05aaf79fe39569994bc04f467353089ec5a31a8
Fareed <sistthelord@gmail.com>
2024-07-10 23:20:54 -0400

    Add circles on corners
    Add margin around preview so the full circle is clickable.
    Without margin 3/4 of circle is outside the overlay so no click is registered on majority of the circle.

--- src/ui/crop_box.rs
+++ src/ui/crop_box.rs
@@ -8,6 +8,9 @@ use gtk4::subclass::widget::WidgetImpl;
 use gtk4::{gdk, glib, graphene, gsk, Snapshot};
 use relm4::gtk;
 
+// todo: better name. RADIUS, INSTEP, MARGIN
+pub static OFFSET: f32 = 4.;
+
 #[derive(Debug, Clone, Copy)]
 pub enum CropType {
     CropFree = 0,
@@ -42,8 +45,11 @@ impl WidgetImpl for CropBoxWidget {
     fn snapshot(&self, snapshot: &Snapshot) {
         let widget = self.obj();
 
-        let border_rect =
-            graphene::Rect::new(0., 0., widget.width() as f32, widget.height() as f32);
+        // removing margin from dimensions to get actual video dimensions
+        let width = widget.width() as f32 - (OFFSET * 2.);
+        let height = widget.height() as f32 - (OFFSET * 2.);
+
+        let border_rect = graphene::Rect::new(OFFSET, OFFSET, width, height);
 
         let border = gsk::RoundedRect::from_rect(border_rect, 0.);
         let border_widths = [1.; 4];
@@ -51,28 +57,42 @@ impl WidgetImpl for CropBoxWidget {
 
         let thirds_box_stroke = gsk::Stroke::builder(1.).build();
 
-        let horizontal_step = (widget.width() / 3) as f32;
+        let horizontal_step = width / 3.;
         for step in 1..3 {
             let x = horizontal_step * step as f32;
 
             let path_builder = gsk::PathBuilder::new();
-            path_builder.move_to(x, 0.);
-            path_builder.line_to(x, widget.height() as f32);
+            path_builder.move_to(x, OFFSET);
+            path_builder.line_to(x, height);
 
             let line = path_builder.to_path();
             snapshot.append_stroke(&line, &thirds_box_stroke, &gdk::RGBA::GREEN);
         }
 
-        let vertical_step = (widget.height() / 3) as f32;
+        let vertical_step = height / 3.;
         for step in 1..3 {
             let y = vertical_step * step as f32;
 
             let path_builder = gsk::PathBuilder::new();
-            path_builder.move_to(0., y);
-            path_builder.line_to(widget.width() as f32, y);
+            path_builder.move_to(OFFSET, y);
+            path_builder.line_to(width, y);
 
             let line = path_builder.to_path();
             snapshot.append_stroke(&line, &thirds_box_stroke, &gdk::RGBA::GREEN);
+        }
+
+        let circle_points = [
+            graphene::Point::new(OFFSET, OFFSET),
+            graphene::Point::new(OFFSET, height + OFFSET),
+            graphene::Point::new(width + OFFSET, OFFSET),
+            graphene::Point::new(width + OFFSET, height + OFFSET),
+        ];
+
+        for point in circle_points {
+            let path_builder = gsk::PathBuilder::new();
+            path_builder.add_circle(&point, OFFSET);
+            let circle = path_builder.to_path();
+            snapshot.append_fill(&circle, gsk::FillRule::Winding, &gdk::RGBA::GREEN);
         }
 
         snapshot.append_border(&border, &border_widths, &border_colours);
--- src/ui/video_player.rs
+++ src/ui/video_player.rs
@@ -14,7 +14,7 @@ use gtk4::prelude::{BoxExt, ButtonExt, OrientableExt, WidgetExt};
 use relm4::adw::gdk;
 use relm4::*;
 
-use crate::ui::crop_box::CropType;
+use crate::ui::crop_box::{CropType, OFFSET};
 use crate::ui::timeline::{TimelineModel, TimelineMsg, TimelineOutput};
 
 struct PlayingInfo {
@@ -92,6 +92,12 @@ impl Component for VideoPlayerModel {
                 add_overlay: crop_box = &super::CropBoxWidget::default(){
                     #[watch]
                     set_visible: model.show_crop_box,
+
+                    add_controller = gtk::GestureClick {
+                        connect_pressed[sender] => move |_,_,x,y| {
+                            println!("cropBox click at {x}, {y}");
+                        }
+                    },
                 },
             },
 
@@ -148,6 +154,7 @@ impl Component for VideoPlayerModel {
         let picture = gtk::Picture::new();
 
         picture.set_paintable(Some(&paintable));
+        picture.set_margin_all(OFFSET as i32);
 
         let offload = gtk4::GraphicsOffload::new(Some(&picture));
         offload.set_enabled(gtk::GraphicsOffloadEnabled::Enabled);
