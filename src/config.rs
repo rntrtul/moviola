a15f5f6d53840de73512cd7a48c268683c5bbad4
Fareed <sistthelord@gmail.com>
2024-08-07 16:19:10 -0400

    Set project to build with meson and to flatpak.

---
+++ src/config.rs
@@ -0,0 +1,4 @@
+pub static VERSION: &str = "0.1.0";
+pub static GETTEXT_PACKAGE: &str = "moviola";
+pub static LOCALEDIR: &str = "/app/share/locale";
+pub static PKGDATADIR: &str = "/app/share/moviola";
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-use relm4::{adw, RelmApp, RELM_THREADS};
+use relm4::{adw, gtk, RelmApp, RELM_THREADS};
 
 use crate::app::App;
 
 mod app;
+mod config;
 mod ui;
 mod video;
 
 fn main() {
     gst::init().unwrap();
+    gtk::init().unwrap();
     RELM_THREADS.set(2).unwrap();
-    relm4_icons::initialize_icons();
+    // relm4_icons::initialize_icons();
     let style_manger = adw::StyleManager::default();
     style_manger.set_color_scheme(adw::ColorScheme::ForceDark);
 
-    let app = RelmApp::new("relm4.test.videditor");
+    let app = RelmApp::new("org.fareedsh.Moviola");
     app.run::<App>(0);
 }
