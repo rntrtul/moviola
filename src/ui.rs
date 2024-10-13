1c8d2e15ee8a00649144d0b7a9a7096c7f577a46
Fareed <sistthelord@gmail.com>
2024-10-13 17:44:22 -0400

    Change timeline overlay colour to be pure black with alpha.

--- src/ui.rs
+++ src/ui.rs
@@ -7,8 +7,7 @@ pub(crate) mod sidebar;
 pub mod timeline;
 pub mod video_player;
 
-// todo: seems too grey. More noticeable on crop overlay
-pub(crate) static IGNORE_OVERLAY_COLOUR: gdk::RGBA = gdk::RGBA::new(0.612, 0.612, 0.612, 0.79);
+pub(crate) static IGNORE_OVERLAY_COLOUR: gdk::RGBA = gdk::RGBA::new(0.0, 0.0, 0.0, 0.7);
 
 glib::wrapper! {
     pub struct HandleWidget(ObjectSubclass<handle::HandleWidget>)
